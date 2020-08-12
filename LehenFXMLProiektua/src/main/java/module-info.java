f8a919f4c68149c754a36197975f9943770c15dd
Idoia Portatiletik <imadariaga@uni.eus>
2020-08-12 16:20:23 +0200

    Aurtengo lehen proiektua, (Swing ez, baizik eta JavaFX erabilita)

---
+++ LehenFXMLProiektua/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module eus.uni.dam2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens eus.uni.dam2 to javafx.fxml;
+    exports eus.uni.dam2;
+}
+*/
